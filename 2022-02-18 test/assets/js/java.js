06c3f6de3db00b850d49d3a90bbf38ee1fa96211
Armandaszev <97019349+Armandaszev@users.noreply.github.com>
2022-02-18 10:42:43 +0200

    Add files via upload
    2022-02-18 testas

---
+++ 2022-02-18 test/assets/js/java.js
@@ -0,0 +1,39 @@
+function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  // 2 uzduotis.
+
+//   for ( let i = 1; i <= 10; i++) {
+//       console.log(i);
+//   }
+
+  // 3 uzduotis.
+
+//   for ( let i = 0; i <= 10; i++) {
+//       if (i % 2 == 0)
+//       console.log(i)
+//   }
+
+  // 4 uzduotis.
+
+//   let skaiciai = 0;
+
+//   for ( let i = 0; i < 10; i++) {
+//       let skaiciai = random(1, 10)
+
+//       console.log(skaiciai)
+//   }
+
+  // 5 uzduotis.
+
+//   let skaicius1 = random()
+
+//   while (skaicius1 != 5) {
+//       skaicius1 = random (1, 10)
+//       console.log(skaicius1)
+//   }
+
+// NEISMOKES...........
